81fa622c88d5f745688a700ab7ce1dc79c66d6ef
Benidiktus Verdo Daviarta <128133583+verdo123@users.noreply.github.com>
2024-02-23 16:53:58 +0700

    commit 23/02/24

--- src/test/java/Mrtj.java
+++ src/test/java/Mrtj.java
@@ -1,3 +1,4 @@
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import org.example.Utils;
@@ -8,6 +9,7 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 public class Mrtj {
     AndroidDriver driver;
@@ -36,19 +38,32 @@ public class Mrtj {
     @Test
     public void testMrtj(){
         Utils.delay(5);
-        MobileElement serviceSearch = (MobileElement) driver.findElementByXPath("//android.widget.TextView[@text='Looking for Services?']");
+        MobileElement serviceSearch = (MobileElement) driver.findElementById("id.go.jakarta.smartcity.jaki:id/search_view");
         serviceSearch.click();
-        Utils.delay(2);
+        Utils.delay(5);
         MobileElement services = (MobileElement) driver.findElementById("id.go.jakarta.smartcity.jaki:id/search_view");
         services.sendKeys("public transport");
         Utils.delay(5);
-        MobileElement publicTransport = (MobileElement) driver.findElementByXPath("//androidx.recyclerview.widget.RecyclerView[@resource-id='id.go.jakarta.smartcity.jaki:id/search_result_recycler']/android.widget.LinearLayout[2]");
+        MobileElement publicTransport = (MobileElement) driver.findElementById("id.go.jakarta.smartcity.jaki:id/info_view");
         publicTransport.click();
         Utils.delay(5);
         MobileElement mrtj = (MobileElement) driver.findElementById("id.go.jakarta.smartcity.jaki:id/item_view");
         mrtj.click();
         Utils.delay(5);
+        MobileElement route = (MobileElement) driver.findElementById("id.go.jakarta.smartcity.jaki:id/code_view");
+        route.click();
 
+        List<MobileElement> listOfRoute = driver.findElements(MobileBy.xpath("(//div[@class='android.widget.LinearLayout']"));
+
+        for (int i = 0; i < listOfRoute.size(); i++) {
+            System.out.println(listOfRoute.get(i).getText());
+
+        }
+
+
+//        MobileElement route = (MobileElement) driver.findElementByXPath("//android.widget.LinearLayout[@resource-id='id.go.jakarta.smartcity.jaki:id/expanded_header']");
+//        String txtRoute = route.getText();
+//        System.out.println(txtRoute);
     }
 
 }
